2df10879cbee4cc01c80e34a249eb20fe74aa9cd
Sebastian Wilwerth <sebastian.wilwerth@gmail.com>
2020-05-11 13:16:15 -0300

    amplitude quantization version

--- tests/ZMQTests.hpp
+++ tests/ZMQTests.hpp
@@ -8,7 +8,7 @@
 
 
 TEST_CASE( "ZMQ API", "[ZMQ][SDL2][GNURadio]") {
-
+/*
     SECTION( "ZMQ REQ/REP complex pipe" ) {
         ZMQVideoPipe zPipe;
         for(int i = 1e2; i > 0; --i)
@@ -20,7 +20,7 @@ TEST_CASE( "ZMQ API", "[ZMQ][SDL2][GNURadio]") {
         for(int i = 1e2; i > 0; --i)
             zPipe.testPassThruQuant();
     }
-    
+  */
     SECTION( "ZMQ REQ Source" ) {
         ZMQVideoPipe zPipe;
         SDL_Surface* sample = Loader::AllocateSurface( Config::NKERNEL_WIDTH, Config::NKERNEL_HEIGHT );
@@ -29,7 +29,7 @@ TEST_CASE( "ZMQ API", "[ZMQ][SDL2][GNURadio]") {
         SDL_SaveBMP(sample, "encoded.bmp");
         SDL_FreeSurface(sample);
     }
-
+/*
 
     SECTION( "GNURadio surface to complex, complex to surface") {
         ZMQVideoPipe zPipe;
@@ -49,13 +49,61 @@ TEST_CASE( "ZMQ API", "[ZMQ][SDL2][GNURadio]") {
         SDL_FreeSurface(recover);
     }
 
-
+*/
     SECTION( "ZMQ REP Sink" ) {
         ZMQVideoPipe zPipe;
         SDL_Surface* sample = Loader::AllocateSurface( Config::NKERNEL_WIDTH, Config::NKERNEL_HEIGHT );
-        sample = SDL_LoadBMP("encoded.bmp");
-        for (int i=20; i > 0; --i)
+
+        sample = SDL_LoadBMP("loop.bmp");
+        for (int i=3; i > 0; --i)
             zPipe.testSendFrame( sample );
+
+        sample = SDL_LoadBMP("loop_backwards.bmp");
+        for (int i=3; i > 0; --i)
+            zPipe.testSendFrame( sample );
+
+        sample = SDL_LoadBMP("loop_copied.bmp");
+        for (int i=3; i > 0; --i)
+            zPipe.testSendFrame( sample );
+
+        sample = SDL_LoadBMP("loop_blurry.bmp");
+        for (int i=3; i > 0; --i)
+            zPipe.testSendFrame( sample );
+
+        sample = SDL_LoadBMP("loop_modulate.bmp");
+        for (int i=3; i > 0; --i)
+            zPipe.testSendFrame( sample );
+
+        sample = SDL_LoadBMP("loop_flop.bmp");
+        for (int i=3; i > 0; --i)
+            zPipe.testSendFrame( sample );
+
+        sample = SDL_LoadBMP("loop_negate.bmp");
+        for (int i=3; i > 0; --i)
+            zPipe.testSendFrame( sample );
+
+        sample = SDL_LoadBMP("loop_flipflop.bmp");
+        for (int i=3; i > 0; --i)
+            zPipe.testSendFrame( sample );
+
+        /*
+        sample = SDL_LoadBMP("loop_dropped.bmp");
+        for (int i=3; i > 0; --i)
+            zPipe.testSendFrame( sample );
+*/
+ /*
+        sample = SDL_LoadBMP("loop_noisy.bmp");
+        for (int i=3; i > 0; --i)
+            zPipe.testSendFrame( sample );
+
+        sample = SDL_LoadBMP("loop_reversed.bmp");
+        for (int i=3; i > 0; --i)
+            zPipe.testSendFrame( sample );
+
+        sample = SDL_LoadBMP("loop_blurry.bmp");
+        for (int i=3; i > 0; --i)
+            zPipe.testSendFrame( sample );
+*/
         SDL_FreeSurface(sample);
     }
 
@@ -64,7 +112,7 @@ TEST_CASE( "ZMQ API", "[ZMQ][SDL2][GNURadio]") {
         for(int i = 1e2; i > 0; --i)
             zPipe.testFramePassThru();
     }
-    
+
 }
 
 #endif //SDL_CRT_FILTER_ZMQTESTS_HPP
